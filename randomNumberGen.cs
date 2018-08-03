2ad34819f4985629921417b4ce88dbb0b92cc047
Daniel <Boothemav2@yahoo.com>
2018-08-03 12:45:13 -0500

    Add files via upload

---
+++ randomNumberGen.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace randomPersonGenerator
+{
+    class randomNumberGen
+    {
+        string myDocPath =
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        public List<int> randomNumber()
+        {
+            #region Local Veriables
+            List<string> employeeList = new List<string>();
+            int randomInt = 0;
+            List<int> listOfRandomInts = new List<int>();
+            employeeList = File.ReadAllLines(myDocPath + @"\Employee List.txt").ToList();
+            int count = 1;
+            int amount = 1;
+            Random rndm = new Random();
+            int minValue = 1;
+            int maxValue = employeeList.Capacity + 1;
+            #endregion
+
+            #region Number of employee(s) draw
+            Console.Write("\nEnter the number of employees to draw: ");
+            try
+            {
+                amount = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (InvalidCastException)
+            {
+                Console.Write("\nError! Please enter a number.\n");
+            }
+            catch (Exception e)
+            {
+                Console.Write($"\n {e} \n");
+            }
+            
+            while (minValue > amount || maxValue < amount)
+            {
+                Console.WriteLine($"\nAmount picked can't be lower than {minValue} and can't be higher than {maxValue}");
+                Console.Write("\nEnter the number of employees to draw: ");
+                try
+                {
+                    amount = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (InvalidCastException)
+                {
+                    Console.Write("\nError! Please enter a number.\n");
+                }
+                catch (Exception e)
+                {
+                    Console.Write($"\n {e} \n");
+                }
+            }
+            #endregion
+
+            #region Random number(s) picked between min and max values
+            if (amount <= 1)
+            {
+                randomInt = rndm.Next(minValue, maxValue);
+
+                listOfRandomInts.Add(randomInt);
+            }
+
+            if (amount >= 2)
+            {
+                randomInt = rndm.Next(minValue, maxValue);
+                listOfRandomInts.Add(randomInt);
+                count++;
+                while (amount >= count)
+                {
+                    randomInt = rndm.Next(minValue, maxValue);
+
+                    while (listOfRandomInts.Contains(randomInt))
+                    {
+                        randomInt = rndm.Next(minValue, maxValue);
+                        //loopsRan++;
+                    }
+
+                    listOfRandomInts.Add(randomInt);
+
+                    count++;
+                }
+            }
+            #endregion
+
+            #region Output number(s) to cmd
+            foreach (int key in listOfRandomInts)
+            {
+                Console.WriteLine("\n" + key);
+            }
+            #endregion
+
+            return listOfRandomInts;
+        }
+    }
+}
