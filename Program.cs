6bd4fa970c62b2e96434581ac2354936f1824a44
Daniel <Boothemav2@yahoo.com>
2018-08-07 19:10:18 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace randomPersonGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            randomNumberGen rNG = new randomNumberGen();
+            List<int> listOfInts = rNG.randomNumber();
+
+            writeDataToFile wDTF = new writeDataToFile();
+            wDTF.fileCheck(listOfInts);
+
+
+            Console.Write("\nPress any key to exit program.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ writeDataToFile.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace randomPersonGenerator
+{
+    class writeDataToFile
+    {
+        List<string> employeeList = new List<string>();
+        string myDocPath =
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        // File check
+        public void fileCheck(List<int> randomInts)
+        {
+            if (File.Exists(myDocPath + @"\Drug Test.txt") == false)
+            {
+                employeeList = readEmployeeList();
+                writeToFIle(randomInts);
+            }
+            else if (File.Exists(myDocPath + @"\Drug Test.txt") == true)
+            {
+                employeeList = readEmployeeList();
+                appendToFile(randomInts);
+            }
+            else
+            {
+                Console.WriteLine("Something has gone wrong in the program");
+            }
+        }
+
+        public List<string> readEmployeeList()
+        {
+            List<string> employeeList = new List<string>();
+            try
+            {
+                employeeList = File.ReadAllLines(myDocPath + @"\Employee List.txt").ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e + "\n");
+            }
+
+            return employeeList;
+        }
+
+        void writeToFIle(List<int> randomIntList)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(myDocPath + @"\Drug Test.txt"))
+                {
+                    sw.WriteLine($"{Environment.UserName}   {DateTime.Now.ToString("g")}");
+                    foreach (int key in randomIntList)
+                    {
+                        sw.WriteLine($"\n{employeeList.ElementAt(key - 1)}: \t{key}");
+                    }
+                    sw.WriteLine("\n");
+
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            { Console.WriteLine("\n" + e + "\n"); }
+        }
+
+        void appendToFile(List<int> randomIntList)
+        {
+            try
+            {
+                using (StreamWriter sw = File.AppendText(myDocPath + @"\Drug Test.txt"))
+                {
+                    sw.WriteLine($"{Environment.UserName}   {DateTime.Now.ToString("g")}");
+                    foreach (int key in randomIntList)
+                    {
+                        sw.WriteLine($"\n{employeeList.ElementAt(key - 1).ToString()}: \t{key}");
+                    }
+                    sw.WriteLine("\n");
+
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            { Console.WriteLine("\n" + e + "\n"); }
+        }
+    }
+}
+
