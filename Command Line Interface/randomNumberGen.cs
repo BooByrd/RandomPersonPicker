c2ed8ef5ec34397d8cfa2b0afa9a50c08301e57e
Daniel <Boothemav2@yahoo.com>
2019-01-10 20:20:39 -0600

    Add files via upload

---
+++ Command Line Interface/randomNumberGen.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace randomPersonGenerator
+{
+    class randomNumberGen
+    {
+        string myDocPath =
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        public List<int> randomNumber()
+        {
+            #region Local Veriables
+            List<string> peopleList = new List<string>();
+            try
+            {
+                peopleList = File.ReadAllLines(myDocPath + @"\People List.txt").ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e + "\n");
+            }
+            int randomInt = 0;
+            List<int> listOfRandomInts = new List<int>();            
+            int count = 1;
+            int amount = 0;
+            Random rndm = new Random();
+            int minValue = 1;
+            int maxValue = peopleList.Count + 1;
+            #endregion
+
+            #region Number of people to draw
+            Console.Write("\nEnter the number of people to draw: ");
+
+            try
+            {
+                amount = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (InvalidCastException)
+            {
+                Console.Write("\nError! Please enter a number.\n");
+            }
+            catch (Exception e)
+            {
+                Console.Write($"\n {e} \n");
+            }
+            
+            while (minValue > amount || maxValue - 1 < amount)
+            {
+                Console.WriteLine($"\nAmount picked can't be lower than {minValue} and can't be higher than {maxValue - 1}");
+                Console.Write("\nEnter the number of people to draw: ");
+                try
+                {
+                    amount = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (InvalidCastException)
+                {
+                    Console.Write("\nError! Please enter a number.\n");
+                }
+                catch (Exception e)
+                {
+                    Console.Write($"\n {e} \n");
+                }
+            }
+            #endregion
+
+            #region Random number(s) picked between min and max values
+            if (amount <= 1)
+            {
+                randomInt = rndm.Next(minValue, maxValue);
+
+                listOfRandomInts.Add(randomInt);
+            }
+
+            if (amount >= 2)
+            {
+                randomInt = rndm.Next(minValue, maxValue);
+                listOfRandomInts.Add(randomInt);
+                count++;
+                while (amount >= count)
+                {
+                    randomInt = rndm.Next(minValue, maxValue);
+
+                    while (listOfRandomInts.Contains(randomInt))
+                    {
+                        randomInt = rndm.Next(minValue, maxValue);
+                    }
+
+                    listOfRandomInts.Add(randomInt);
+
+                    count++;
+                }
+            }
+            #endregion
+
+            return listOfRandomInts;
+        }
+    }
+}
---
+++ Command Line Interface/writeDataToFile.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace randomPersonGenerator
+{
+    class writeDataToFile
+    {
+        List<string> peopleList = new List<string>();
+        string myDocPath =
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        // File check
+        public void fileCheck(List<int> randomInts)
+        {
+            if (File.Exists(myDocPath + @"\Choosen People.txt") == false)
+            {
+                peopleList = readPeopleList();
+                writeToFIle(randomInts);
+            }
+            else if (File.Exists(myDocPath + @"\Choosen People.txt") == true)
+            {
+                peopleList = readPeopleList();
+                appendToFile(randomInts);
+            }
+            else
+            {
+                Console.WriteLine("Something has gone wrong in the program");
+            }
+        }
+
+        public List<string> readPeopleList()
+        {
+            List<string> peopleList = new List<string>();
+            try
+            {
+                peopleList = File.ReadAllLines(myDocPath + @"\People List.txt").ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e + "\n");
+            }
+
+            return peopleList;
+        }
+
+        void writeToFIle(List<int> randomIntList)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(myDocPath + @"\Choosen People.txt"))
+                {
+                    sw.WriteLine($"{Environment.UserName}   {DateTime.Now.ToString("g")}");
+                    foreach (int key in randomIntList)
+                    {
+                        sw.WriteLine($"\n{peopleList.ElementAt(key - 1)}: \t{key}");
+                        Console.WriteLine($"\n{peopleList.ElementAt(key - 1)}: \t{key}");
+                    }
+                    sw.WriteLine("\n");
+
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            { Console.WriteLine("\n" + e + "\n"); }
+        }
+
+        void appendToFile(List<int> randomIntList)
+        {
+            try
+            {
+                using (StreamWriter sw = File.AppendText(myDocPath + @"\Choosen People.txt"))
+                {
+                    sw.WriteLine($"{Environment.UserName}   {DateTime.Now.ToString("g")}");
+                    foreach (int key in randomIntList)
+                    {
+                        sw.WriteLine($"\n{peopleList.ElementAt(key - 1).ToString()}: \t{key}");
+                        Console.WriteLine($"\n{peopleList.ElementAt(key - 1)}: \t{key}");
+                    }
+                    sw.WriteLine("\n");
+
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            { Console.WriteLine("\n" + e + "\n"); }
+        }
+    }
+}
+
